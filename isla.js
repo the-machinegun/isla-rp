741a702f657a3a8ee1bcaa666ef486d7a6c6bb47
the-machinegun <the.shaina@gmail.com>
2026-09-05 16:35:42 -0500

    Update isla.js

--- isla.js
+++ isla.js
@@ -1,1 +1,12 @@
+function fitLyric(el, size) {
+    while (size > 8) {
+        el.style.fontSize = size + 'px';
+        if (el.scrollWidth <= 325) break;
+        size--;
+    }
+}
 
+document.querySelectorAll('.isla-top, .isla-bottom')
+    .forEach(el => fitLyric(el, 120));
+
+fitLyric(document.querySelector('.isla-seam'), 220);
